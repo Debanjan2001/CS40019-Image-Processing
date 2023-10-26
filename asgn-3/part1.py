77071aeea6d143bdfe7eeb637049aa5b56a45eb6
Debanjan Saha <debanjansaha2001@gmail.com>
2023-10-26 11:57:19 +0530

    Low pass filter

--- asgn-3/part1.py
+++ asgn-3/part1.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 class Part1Processor:
 
@@ -10,11 +11,32 @@ class Part1Processor:
         self.gray_image = cv2.imread(self.image_path, cv2.IMREAD_GRAYSCALE)
         return self.gray_image
     
+    def apply_low_pass_filter(self, image, kernel_size):
+        # Define the kernel for the low-pass filter
+        height, width = image.shape
+        result = np.zeros((height, width), dtype=np.uint8)
+        
+        offset = kernel_size // 2
+        
+        for i in range(offset, height - offset):
+            for j in range(offset, width - offset):
+                result[i, j] = np.mean(image[i-offset:i+offset+1, j-offset:j+offset+1])
+        
+        return result
+
 
 def main():
-    image_path = 'restore.png'
+    image_path = 'restore.jpg'
     part1Solver = Part1Processor()
     gray_image = part1Solver.read_image(image_path)
+
+    kernel_size = 19
+    # Apply the low-pass filter
+    blurred_image = part1Solver.apply_low_pass_filter(gray_image, kernel_size)
+
+    cv2.imshow('Image', blurred_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
